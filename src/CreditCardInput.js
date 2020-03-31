252bb898d43da1a4adab27a13597bb5d648c20d2
Luca Palermo <lucaapalermo@gmail.com>
2020-03-30 22:30:27 -0700

    again

--- src/CreditCardInput.js
+++ src/CreditCardInput.js
@@ -97,7 +97,7 @@ export default class CreditCardInput extends Component {
   componentDidMount = () => this._focus(this.props.focused);
 
   componentWillReceiveProps = newProps => {
-    // if (this.props.focused !== newProps.focused) this._focus(newProps.focused);
+    if (this.props.focused !== newProps.focused) this._focus(newProps.focused);
   };
 
   _focus = field => {
